737ec9e607a635f0437f3cc5990aabaa65a74653
rico <ricoyangjing@didichuxing.com>
2022-04-23 14:42:04 +0800

    update: change datasource to jisilu

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from ics import Calendar, DisplayAlarm, Event
 
 
 def main():
-    url = "http://dcfm.eastmoney.com/em_mutisvcexpandinterface/api/js/get?type=KZZ_LB2.0&token=70f12f2f4f091e459a279469fe49eca5&p=1&st=STARTDATE&sr=-1&ps=50"
+    url = "https://www.jisilu.cn/data/calendar/get_calendar_data/?qtype=CNV"
     print(url)
     try:
         header = {
@@ -22,63 +22,53 @@ def main():
     except Exception as err:
         print(err)
 
-    subscribe_json_data = [] # 所有还未上市的可转债
-    for data in json_data:
-        now_date = datetime.now().date()        
-        LISTDATE = data['LISTDATE'].split('T')[0] 
-        if LISTDATE == '-': 
-            LISTDATE = now_date
-        else:
-            LISTDATE = datetime.strptime(LISTDATE, '%Y-%m-%d').date()
-                
-        if LISTDATE < now_date:
-            # print("可转债信息过期！")
-            continue
-        subscribe_json_data.append(data)
-
     bond_subscribe_data = []
-    for data in subscribe_json_data:
+    for data in json_data:
         dict = {}
         try:
-            dict['STARTDATE'] = data['STARTDATE']  # 申购日期
-            dict['LISTDATE'] = data['LISTDATE']  # 上市日期 可能为 -
-            dict['BONDCODE'] = data['BONDCODE']  # 债券代码
-            dict['SNAME'] = data['SNAME']  # 债券简称
-            dict['CORRESCODE'] = data['CORRESCODE']  # 申购代码
-            dict['ZGJ'] = data['ZGJ']  # 正股价
-            dict['SWAPPRICE'] = data['SWAPPRICE']  # 转股价
-            dict['ZGPRICE'] = (100 * float(dict['ZGJ'])) / float(dict['SWAPPRICE'])  # 转股价值
-            # 债现价
-            # 转股溢价率
+            dict['id'] = data['id']
+            dict['code'] = data['code']
+            dict['title'] = data['title']
+            dict['start'] = data['start']
+            dict['description'] = data['description']
+            dict['url'] = data['url']
+            dict['color'] = data['color']
             bond_subscribe_data.append(dict)
         except Exception as err:
             print(err)
 
+    start_bonds_data = [] #待申购的可转债
+    list_bonds_data = [] # 待上市的可转债    
+    for dict in bond_subscribe_data:
+        if "申购日" in dict['title']:
+            start_bonds_data.append(dict)
+        if "上市日" in dict['title']:
+            list_bonds_data.append(dict)
+
     print('整理筛选后的申购数据:\n')
     c = Calendar()
-    for d in bond_subscribe_data:
-        listdate = d['LISTDATE'] #2020-10-21T00:00:00       
-        if listdate != '-':
-            le = Event()
-            le.name = d['SNAME'] +"上市"
-            ldate = listdate.replace("T"," ") #'2014-01-01 00:00:00' 8:00
-            le.begin = ldate.replace("00:00:00", "01:30:00") # 9:30
-            le.end = ldate.replace("00:00:00", "03:30:00") # 9:30
-            le.alarms.append(DisplayAlarm(trigger=timedelta(minutes=-5), display_text=le.name))
-            le.alarms.append(DisplayAlarm(trigger=timedelta(seconds=-10), display_text=le.name))
-            le.description = 'http://data.eastmoney.com/kzz/detail/%s.html' % (d['BONDCODE'])
-            c.events.add(le)
+    for d in list_bonds_data:
+        listdate = d['start'] #2022-04-25
+        le = Event()
+        le.name = d['title']
+        ldate = listdate + " 00:00:00" #'2022-04-25 00:00:00' 8:00
+        le.begin = ldate.replace("00:00:00", "01:30:00") # 9:30
+        le.end = ldate.replace("00:00:00", "03:30:00") # 9:30
+        le.alarms.append(DisplayAlarm(trigger=timedelta(minutes=-5), display_text=le.name))
+        le.alarms.append(DisplayAlarm(trigger=timedelta(seconds=-10), display_text=le.name))
+        le.description = 'http://jisilu.cn/data/convert_bond_detail/%s' % (d['code'])
+        c.events.add(le)
 
-        startdate = d['STARTDATE']
-        if startdate != '-':
-            se = Event()
-            se.name = d['SNAME'] + "申购"
-            sdate = startdate.replace("T"," ") #'2014-01-01 00:00:00' 8:00
-            se.begin = sdate.replace("00:00:00", "01:30:00") # 9:30
-            se.end = sdate.replace("00:00:00", "03:30:00")  # 11:30
-            se.alarms.append(DisplayAlarm(trigger=timedelta(minutes=150), display_text=se.name))# 12:00
-            se.description = 'http://data.eastmoney.com/kzz/detail/%s.html' % (d['BONDCODE'])
-            c.events.add(se)
+    for d in start_bonds_data:
+        startdate = d['start']
+        se = Event()
+        se.name = d['title']
+        sdate = startdate + " 00:00:00" #'2022-04-25 00:00:00' 8:00
+        se.begin = sdate.replace("00:00:00", "01:30:00") # 9:30
+        se.end = sdate.replace("00:00:00", "03:30:00")  # 11:30
+        se.alarms.append(DisplayAlarm(trigger=timedelta(minutes=150), display_text=se.name))# 12:00
+        se.description = 'http://jisilu.cn/data/convert_bond_detail/%s' % (d['code'])
+        c.events.add(se)
         
     with open('kzz.ics', 'w') as my_file:
         my_file.writelines(c)
